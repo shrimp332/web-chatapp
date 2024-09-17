734fea6eec330f39e1d174aeb96f17de9c65f3f3
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-09-17 17:02:24 +1000

    add input sanitization

--- internal/handlers/message.go
+++ internal/handlers/message.go
@@ -3,14 +3,41 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
-	message := r.FormValue("chatinput")
+	message, err := santizeInput(r.FormValue("chatinput"))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 	resp := fmt.Sprintf(`
 		<div class="chat-box" id="chatbox" hx-swap-oob="afterbegin:#chatbox"><span class="chat-message">%s</span></div>
 		<input hx-post="/message" autofocus hx-swap="outerHTML" class="chat-input" type="text" name="chatinput" value="">
 	`, message)
 
 	w.Write([]byte(resp))
+}
+
+func santizeInput(s string) (string, error) {
+	subs := map[string]string{
+		"&":  "&amp;",
+		"<":  "&lt;",
+		">":  "&gt;",
+		"\"": "&quot;",
+		"'":  "&#x27;",
+		"/":  "&#x2F;",
+	}
+	pattern := "/|&|<|>|\"|'"
+
+	reg, err := regexp.Compile(pattern)
+	if err != nil {
+		return "", err
+	}
+
+	return reg.ReplaceAllStringFunc(s, func(match string) string {
+		return subs[match]
+	}), nil
 }
