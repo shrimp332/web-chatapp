562874c35a79b98f8c9f324729775fda2f1e02a1
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-09-17 16:37:39 +1000

    add add message to list

--- internal/handlers/message.go
+++ internal/handlers/message.go
@@ -1,13 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write(
-		[]byte(
-			`<input hx-post="/message" autofocus hx-swap="outerHTML" class="chat-input" type="text" name="chatinput" value="">`,
-		),
-	)
+	message := r.FormValue("chatinput")
+	resp := fmt.Sprintf(`
+		<div class="chat-box" id="chatbox" hx-swap-oob="afterbegin:#chatbox"><span class="chat-message">%s</span></div>
+		<input hx-post="/message" autofocus hx-swap="outerHTML" class="chat-input" type="text" name="chatinput" value="">
+	`, message)
+
+	w.Write([]byte(resp))
 }
